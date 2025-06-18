5f73d4447efee1b0bc0a02703d4913215e35b867
Ashish Lukka <ashishlukka2005@gmail.com>
2025-06-18 14:26:34 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://skill-caravan.vercel.app"
+    "https://olive-skill-test.vercel.app"
   ],
   credentials: true,
 }));
